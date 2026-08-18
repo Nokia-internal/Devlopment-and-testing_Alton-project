f502acc82933fa0e5c8e8ccad2a7903fc5b6e3f4
raghavpatel220292 <raghav2202@zohomail.in>
2026-08-18 21:45:47 +0530

    prod added

---
+++ environment/prod/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.80.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name          = "prod-rg-remotestatefile"
+    storage_storage_account_name = "prod-onewebstg"
+    container_name               = "prod-oneweb-container"
+    key                          = "prod-oneweb-statefile.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
